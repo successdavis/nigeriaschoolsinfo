471bc0e1ddd832e76e230829af684237c0e94ed3
vagrant <vagrant@homestead>
2020-01-02 12:15:33 +0000

    add watchers table

---
+++ database/migrations/2020_01_02_100207_create_watches_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWatchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('watches', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $this->unsignedInteger('watch_id');
+            $this->string('watch_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('watches');
+    }
+}
