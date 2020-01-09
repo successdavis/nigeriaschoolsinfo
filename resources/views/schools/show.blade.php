58782efb05dc20aac1c846227ae7f583ea67c427
vagrant <vagrant@homestead>
2020-01-09 23:45:17 +0000

    improve readability on school's show page

--- resources/views/schools/show.blade.php
+++ resources/views/schools/show.blade.php
@@ -39,35 +39,38 @@
 					</div>
 				</article>
 				<div class="mb-small">
-					<div class="mb-small"><strong>School Type:</strong> {{$school->typeOf()}} </div>
-					<div><strong>Sponsor:</strong> {{$school->sponsored->name}} </div>
+					<div class="mb-small is-size-5-desktop"><strong>School Type:</strong> {{$school->typeOf()}} </div>
+					<div class="is-size-5-desktop"><strong>Sponsor:</strong> {{$school->sponsored->name}} </div>
 				</div>
 				<div class="mb-small">
-					<div class="is-size-4">{{$school->short_name}} Admission Info: </div>
+					<div class="is-size-3">{{$school->short_name}} Admission Info: </div>
 					@if ($school->isAdmitting())
-						<div>{{$school->short_name}} is open for application to new intake</div>
-						<div>Interested candidates can find below the website address and portal for application, if you have trouble applying, please find below the school contacts information in the contact section below or visit <a target="_blank" href="https://www.sleettech.com">www.sleettech.com</a> for documented steps and procedures to successfully complete your application.</div>
+						<div class="is-size-5-desktop">{{$school->short_name}} is open for application to new intake</div>
+						<div class="is-size-5-desktop">Interested candidates can find below the website address and portal for application, if you have trouble applying, please also find below the school's contact information in the contact section or visit <a target="_blank" href="https://www.sleettech.com">www.sleettech.com</a> for documented steps and procedures to successfully complete your application.</div>
 					@else
-						<div>{{$school->short_name }} is not open for registration at the moment, Please check back later</div>
+						<div class="is-size-5-desktop">{{$school->short_name }} is not open for registration at the moment, Please check back later</div>
 						{{-- put a subscription button here later --}}
 					@endif
 				</div>
 				<div class="mb-small">
-					<div class="is-size-4 ">Description</div>
-					<p>{!! nl2br($school->description) !!}</p>
-					<div class="is-size-4">Contact Information</div>
-					<div>{{$school->name }} is school located in {{$school->state->name}} State, {{$school->lga->name}} Local Government Area.</div>
-					<div class="mt-small"><strong>School Address:</strong> {{$school->address}}</div>
-					<div><strong>Phone Number: </strong> +234{{$school->phone}} </div>
-					<div><strong>Email: </strong> {{$school->email}} </div>
-					<div><strong>Website:</strong><a href="https://{{$school->website}}" target="_blank"> {{$school->website}}</a></div>
-					<div><strong>Portal: </strong><a href="https://{{$school->portal_website}}" target="_blank"> {{$school->portal_website}}</a></div> 
+					<div class="is-size-3 ">Description</div>
+					<p class="is-size-5-desktop">{!! nl2br($school->description) !!}</p>
+					<div class="is-size-3 mt-small">Contact Information</div>
+					<div class="is-size-5-desktop">{{$school->name }} is school located in {{$school->state->name}} State, {{$school->lga->name}} Local Government Area.</div>
+					<div class="is-size-5-desktop">
+						<div class="mt-small"><strong>School Address:</strong> {{$school->address}}</div>
+						<div><strong>Phone Number: </strong> +234{{$school->phone}} </div>
+						<div><strong>Email: </strong> {{$school->email}} </div>
+						<div><strong>Website:</strong><a href="https://{{$school->website}}" target="_blank"> {{$school->website}}</a></div>
+						<div><strong>Portal: </strong><a href="https://{{$school->portal_website}}" target="_blank"> {{$school->portal_website}}</a></div> 
+					</div>
 				{{-- <p>{!! nl2br(str_replace(" ", "&nbsp;", $school->description)) !!}</p> --}}
 				</div>
 				<div class="mt-small">
-					<div class="is-size-4 ">{{$school->name}} list of courses offered:</div>
+					<div class="is-size-3 ">{{$school->name}} list of courses offered:</div>
+					<div class="is-size-5-desktop mb-small">The following is a list of courses offered in {{$school->short_name}} </div>
 					@foreach ($school->courses as $course)
-						<div>{{$course->name}}</div>
+						<div class="is-size-5-desktop">{{$course->name}}</div>
 					@endforeach
 				</div>
 			</div>
