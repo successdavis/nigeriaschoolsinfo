28241104c0d40d67f67a44ea730e35262418dbdf
vagrant <vagrant@homestead>
2020-05-08 22:44:39 +0000

    Fix bug on school courses offered page, change add name

--- resources/views/schools/coursesoffered.blade.php
+++ resources/views/schools/coursesoffered.blade.php
@@ -10,7 +10,7 @@
 @section('content')
 
 	{{-- ==== Google Adsense Display Ads ===== --}}
-    @include ('sections/ads/horitalal_banner')
+    @include ('sections/ads/horizontal_banner')
     
 	<div class="container">
 		<div class="columns mt-medium">
