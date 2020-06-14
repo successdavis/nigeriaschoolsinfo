4ded18615e9a9985816d102b10e3746637e2de71
vagrant <vagrant@homestead>
2020-06-14 21:32:15 +0000

    add notifyfollowusers and compile to production

--- app/Http/Controllers/FollowupController.php
+++ app/Http/Controllers/FollowupController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 
 class FollowupController extends Controller
 {
+    // Followup post are post that display under a particular school, job or model.
+
     public function store(Post $post)
     {
     	$post->markAsFollow();
---
+++ app/Http/Controllers/NotifyfollowusersController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\notifyfollowusers;
+use Illuminate\Http\Request;
+
+class NotifyfollowusersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\notifyfollowusers  $notifyfollowusers
+     * @return \Illuminate\Http\Response
+     */
+    public function show(notifyfollowusers $notifyfollowusers)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\notifyfollowusers  $notifyfollowusers
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(notifyfollowusers $notifyfollowusers)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\notifyfollowusers  $notifyfollowusers
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, notifyfollowusers $notifyfollowusers)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\notifyfollowusers  $notifyfollowusers
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(notifyfollowusers $notifyfollowusers)
+    {
+        //
+    }
+}
