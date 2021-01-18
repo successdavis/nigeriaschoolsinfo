b33ca05cc4c779632e266c7201729b8d6bc1774c
successdavis <simondavid612@gmail.com>
2021-01-18 01:37:25 +0100

    remove return statement on course filter s method

--- app/Filters/CourseFilters.php
+++ app/Filters/CourseFilters.php
@@ -33,9 +33,6 @@ class CourseFilters extends Filters
     
     protected function s($s)
     {
-        if ($s == null) {
-            return;
-        }
 
         return $this->builder->where(function ($query) use ($s) {
             $query->where('name', 'LIKE', "%{$s}%");
