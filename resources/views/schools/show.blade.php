bdd65143d789465140d8b0ba25e2964920edbb16
successdavis <simondavid612@gmail.com>
2021-01-31 18:01:55 +0100

    Fix bug on Schools show page

--- resources/views/schools/show.blade.php
+++ resources/views/schools/show.blade.php
@@ -58,7 +58,7 @@
 					<div class="mb-2 is-size-5-desktop"><strong>Programme:</strong> {{$school->typeOf()}} </div>
 					<div class="mb-3 is-size-5-desktop"><strong>Sponsor:</strong> {{ucfirst($school->sponsored->name)}} </div>
 				<div class="mb-small">
-					<p>{{$school->name}} also known as {{$school->short_name}} is a {{$school->sponsored->name}} {{$school->typeOf()}} established in {{$school->date_created->format('Y')}} ({{$school->date_created->diffForHumans()}}). The school offers over {{$school->courses_count}} undergraduate courses, It is located in {{$school->state->name}}. 
+					<p class="is-size-5-desktop">{{$school->name}} also known as {{$school->short_name}} is a {{$school->sponsored->name}} {{$school->typeOf()}} established in {{$school->date_created->format('Y')}} ({{$school->date_created->diffForHumans()}}). The school offers over {{$school->courses_count}} undergraduate courses, It is located in {{$school->state->name}}. 
 						@if($school->website)
 
 						{{ucfirst($school->short_name)}} has a public profile for anyone who wants to know more about the institution and can be accessed via its website. Visit {{$school->short_name}} landing page on {{$school->website}} or visit the school portal on {{$school->portal_website}} to apply for admission.
@@ -71,13 +71,13 @@
 					Other vital informations such as courses offered, UTME requirement, Direct Entry requirement, and the school photos are available below.
 					</p>
 
-					<h3 class="is-size-4 mt-3">Does {{ucfirst($school->short_name)}} have hostel accommodation</h3>
+					<h3 class="is-size-4 mt-3 ">Does {{ucfirst($school->short_name)}} have hostel accommodation</h3>
 					@if($school->hostels)
-						{{$school->short_name}} has a provision for hostel accomodation to students who desire to live within the school premises. 
+						<p class="is-size-5-desktop">{{$school->short_name}} has a provision for hostel accomodation to students who desire to live within the school premises. </p>
 					@else
-						 No! {{$school->short_name}} does not have provission for hostel accomodation at the moment. 
+						 <p class="is-size-5-desktop">No! {{$school->short_name}} does not have provission for hostel accomodation at the moment. </p>
 					@endif
-					<p class="content">Hostels accomodation is dedicated to making students living stress free and to reduce the cost fee for renting a house outside the school premises.</p>
+					<p class="is-size-5-desktop">Hostels accomodation is dedicated to making students living stress free and to reduce the cost fee for renting a house outside the school premises.</p>
 
 
 				</div>
@@ -86,7 +86,7 @@
 					<div class="is-size-4">{{ucfirst($school->short_name)}} Admission Info: </div>
 					@if ($school->isAdmitting())
 						<div class="is-size-5-desktop">{{$school->short_name}} is open for application to new intake</div>
-						<div class="is-size-5-desktop">Interested candidates can find below the website address and portal for application, if you have trouble applying, use the contact information provided here <a target="_blank" href="https://www.sleettech.com">www.sleettech.com</a> for documented steps and procedures to successfully complete your application.</div>
+						<div class="is-size-5-desktop">Interested candidates can find below the website address and portal for application, if you have trouble applying, do not hesitate to let us know using the comment section provided below or the institution's contact information provided here. Also check the recommended section below to find an article on the steps and procedures to successfully complete your application.</div>
 					@else
 						<div class="is-size-5-desktop">{{$school->short_name }} is not open for POST UTME or Direct Entry application at the moment, Please check back here again</div>
 						{{-- put a subscription button here later --}}
@@ -95,7 +95,7 @@
 
 				<div class="mb-small">
 					<div class="is-size-4 ">Description</div>
-					<p class="content has-text-justified">{!! nl2br($school->description) !!}</p>
+					<p class="is-size-5-desktop has-text-justified">{!! nl2br($school->description) !!}</p>
 
 					<div class="section content">
 						<h3>Recommended Articles</h3>
@@ -105,7 +105,7 @@
 					</div>
 				    
 				    <h3 class="is-size-4">{{ucfirst($school->short_name)}} cut off mark</h3>
-				    <p class="content has-text-justified">The average cut off mark for {{$school->short_name}} is {{$school->jamb_points}}. It is important to note that this cut off mark may not be applicable for all courses, some courses may require you to score higher, while some even lower. <br>
+				    <p class="is-size-5-desktop has-text-justified">The average cut off mark for {{$school->short_name}} is {{$school->jamb_points}}. It is important to note that this cut off mark may not be applicable for all courses, some courses may require you to score higher, while some even lower. <br>
 				    
 				    @include ('sections/ads/in-feed')
 
@@ -113,7 +113,7 @@
 				    </p>
 
 					<div class="is-size-4 mt-small">{{$school->short_name}} School Address?</div>
-					<div class="content has-text-justified">{{$school->name }} is located in {{$school->state->name}} State, {{$school->lga->name}} Local Government Area.</div>
+					<div class="content is-size-5-desktop has-text-justified">{{$school->name }} is located in {{$school->state->name}} State, {{$school->lga->name}} Local Government Area.</div>
 					<div class="is-size-5-desktop">
 						<div class="mt-small"><strong>School Address:</strong> {{$school->address}}</div>
 						<div><strong>Phone Number: </strong> +234{{$school->phone}} </div>
