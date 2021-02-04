3d4804f77b1d3128fa29e980ab975a0c51cc73d0
successdavis <simondavid612@gmail.com>
2021-02-04 13:47:42 +0100

    Refactor post image code to foreach loop

--- app/Http/Controllers/PostImageController.php
+++ app/Http/Controllers/PostImageController.php
@@ -9,7 +9,7 @@ use Intervention\Image\Facades\Image;
 
 class PostImageController extends Controller
 {
-   
+
     public function addimage(Request $request)
     {
         $this->authorize('create', new Post);
@@ -27,77 +27,45 @@ class PostImageController extends Controller
         // The path and format to store the image
         $path = 'public/posts/' . $newName;
         $format = '.webp';
-        $storePath = $path . '-1920px' . $format;
 
-        // Intervention Resize to 1920
-        $img = Image::make(request()->file('file'))->resize(1920, null, function($constraint){
-            $constraint->aspectRatio();
-        });
-        // Store the image to public storage posts directory
-        Storage::put($storePath, (string) $img->encode('webp'));
+        $convertSizes = ['1920','1600','1366','1024','768','640','480'];
 
-        // Convert and store in 1600
-        $storePath = $path . '-1600px' . $format;
-        $img = Image::make(request()->file('file'))->resize(1600, null, function($constraint){
-            $constraint->aspectRatio();
-        });
-        Storage::put($storePath, (string) $img->encode('webp'));
-
-
-        // Convert and store in 1366
-        $storePath = $path . '-1366px' . $format;
-        $img = Image::make(request()->file('file'))->resize(1366, null, function($constraint){
-            $constraint->aspectRatio();
-        });
-        Storage::put($storePath, (string) $img->encode('webp'));
-
-
-        // Convert and store in 1024
-        $storePath = $path . '-1024px' . $format;
-        $img = Image::make(request()->file('file'))->resize(1024, null, function($constraint){
-            $constraint->aspectRatio();
-        });
-        Storage::put($storePath, (string) $img->encode('webp'));
-
-
-        // Convert and store in 768
-        $storePath = $path . '-768px' . $format;
-        $img = Image::make(request()->file('file'))->resize(768, null, function($constraint){
-            $constraint->aspectRatio();
-        });
-        Storage::put($storePath, (string) $img->encode('webp'));
-
-
-
-        // Convert and store in 640
-        $storePath = $path . '-640px' . $format;
-        $img = Image::make(request()->file('file'))->resize(640, null, function($constraint){
-            $constraint->aspectRatio();
-        });
-        Storage::put($storePath, (string) $img->encode('webp'));
+        // Intervention Convert into different sizes
+        foreach ($convertSizes as $size) { 
+            $storePath = $path . '-' .$size .'px' . $format;
+            $img = Image::make(request()->file('file'))->resize($size, null, function($constraint){
+                $constraint->aspectRatio();
+            });
+            Storage::put($storePath, (string) $img->encode('webp'));
+        }
 
         // Return stored path to axios
-        $path = 'posts/' . $newName . '-768px' . $format;
+        $path = 'posts/' . $newName . '-480px' . $format;
 
         $src =  asset('storage/'.$path);
 
-        $srcset =   asset('storage/'.$path) . " 768w," .
-                    asset('storage/posts/' . $newName . '-1600px' . $format) . " 1600w," .
-                    asset('storage/posts/' . $newName . '-1366px' . $format) . " 1366w," .
-                    asset('storage/posts/' . $newName . '-1024px' . $format) . " 1024w," .
-                    asset('storage/posts/' . $newName . '-768px' . $format) . " 768w," .
-                    asset('storage/posts/' . $newName . '-640px' . $format) . " 640w";
+        $srcset =  '';
+        foreach ($convertSizes as $size) {
+           $srcset = $srcset . asset('storage/posts/' . $newName . '-' . $size .'px' . $format) . ' ' . $size .'w,';
+        }
+
+        $sizes =  '';
+        foreach ($convertSizes as $index => $size) {
+            if ($index !== array_key_last($convertSizes)) {
+                $sizes = $sizes . '(max-width: ' . $size . 'px) ' .$size . 'px,';
+            }
+
+            if ($index === array_key_last($convertSizes)) {
+                $sizes = $sizes . '(max-width: ' . $size . 'px) ' .$size . 'px, 1024px';
+            }
+        }
 
         return response()
             ->json([
                 'src' => $src,
                 'alt' => $name,
                 'srcset' => $srcset,
-                'sizes' => "(max-width: 1600px) 1600px, 
-                (max-width: 1366px) 1366px, 
-                (max-width: 1024px) 1024px, 
-                (max-width: 768px) 768px,
-                (max-width: 640px) 640px, 1600px"
+                'sizes' => $sizes,
             ]);
     }
 
